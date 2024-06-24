f30ceaa47193d7508cc454db1eb4b499baac3eb9
diegoolliveira7 <diegoolliveira1999@gmail.com>
2024-06-24 16:42:01 -0300

    criação de novo tipode grapf

---
+++ testeGrapf/chart.js
@@ -0,0 +1,21 @@
+
+const ctx = document.getElementById('barchart'); //se eu deixar myChart, sem colocar o tamanho lá no HTML fica o tamanho q eu quiser
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
